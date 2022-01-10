b488c2184eecbab393bc4f884f741bd624a24410
nesma <nesmazuhier@gmail.com>
2022-01-10 15:57:32 +0200

    navigate to electronic category

--- Scripts/requestQouteScenario/Script1641813640706.groovy
+++ Scripts/requestQouteScenario/Script1641813640706.groovy
@@ -1,27 +1,16 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
-import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
-import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import actions.GeneralActions as GeneralActions
-import actions.HeaderActions as HeaderActions
-import helpers.GeneralHelpers as GeneralHelpers
-import internal.GlobalVariable as GlobalVariable
-import org.openqa.selenium.Keys as Keys
+import actions.GeneralActions
+import actions.HeaderActions
+import helpers.GeneralHelpers
+import validations.CategoryPageValidations
+import validations.GeneralValidations
+
 
 GeneralHelpers.initScenario()
 
 GeneralActions.clickOnAcceptCookies()
 
 HeaderActions.hoverOverProducts()
+HeaderActions.clickOnElectronicVehicleControl()
+GeneralValidations.verifyCurrentPageURL('electronic-vehicle-controls')
+GeneralValidations.verifyCurrentPageTitleValue('Electronic Vehicle Controls')
+CategoryPageValidations.verfiyCurrentPageHeading('Electronic Vehicle Controls')
