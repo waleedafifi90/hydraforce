8878d760f1c64f29754a815f1f8cd7d9a8384bb7
waleedafifi90 <waleedafifi.blog@gmail.com>
2022-01-10 14:06:35 +0200

    init

---
+++ Keywords/validations/GeneralValidations.groovy
@@ -0,0 +1,40 @@
+package validations
+
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+
+import com.kms.katalon.core.annotation.Keyword
+import com.kms.katalon.core.checkpoint.Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.testcase.TestCase
+import com.kms.katalon.core.testdata.TestData
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+
+import internal.GlobalVariable
+
+public class GeneralValidations {
+	/***
+	 * Verify Current Page Title Is Not Empty
+	 * @author waleedafifi 
+	 */
+	public static void verifyCurrentPageTitleIsNotEmpty() {
+		assert !WebUI.getWindowTitle().isEmpty()
+	}
+
+	/**
+	 * Verify Current Page URL matched the passed url
+	 * @param expectedURL
+	 * @author waleedafifi
+	 */
+	public static void verifyCurrentPageURL(String expectedURL) {
+		assert WebUI.getUrl().equals(expectedURL)
+	}
+}
